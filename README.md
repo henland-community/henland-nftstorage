6a0d255d834e07ba995e15d9c82daf15fd82b8d5
ryanlovfer <ryanlovfer@gmail.com>
2023-07-11 10:51:17 +0800

    feat: update README, add .env config files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ipfs-upload-proxy
+
+## 服务信息
+该服务通过 NFT-Storage 存储服务，实现数据上传到 IPFS 网络存储
+服务默认监听端口：4444
+文件默认限制：100M
+
+## 服务运行
+
+``` shell
+npm install
+npm start
+```
---
+++ src/index.mjs
@@ -0,0 +1,70 @@
+import "dotenv/config";
+import { File, NFTStorage } from "nft.storage";
+import { packToBlob } from "ipfs-car/pack/blob";
+import express from "express";
+import { Blob } from "buffer";
+import multer from "multer";
+import cors from "cors";
+import { nanoid } from "nanoid";
+
+const client = new NFTStorage({ token: process.env.API_TOKEN });
+const port = process.env.PORT || 4444;
+const app = express();
+
+app.use(cors());
+
+// debug
+// console.log("--------debug info--------");
+// console.log('PORT', port);
+// console.log('api-token', process.env.API_TOKEN);
+// console.log('file-limit-size', process.env.FILE_SIZE_LIMIT);
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage,
+  preservePath: true,
+  limits: {
+    fileSize: process.env.FILE_SIZE_LIMIT || 100000000, // 100MB
+  },
+});
+
+const handle_error = (res, req, error, status = 500) => {
+  const id = nanoid();
+  console.error(`ERROR from ${req.hostname}@${req.ip} | ${id} -> ${error}`);
+  res.status(status).send(`${id} | ${error}`);
+};
+
+app.post("/single", upload.single("asset"), async function (req, res) {
+  try {
+    if (req.file == null) {
+      return handle_error(res, req, "Invalid request: 'file' is missing.", 400);
+    }
+
+    const { root, car } = await packToBlob({
+      input: new Blob([req.file.buffer]),
+      rawLeaves: false,
+      wrapWithDirectory: false,
+    });
+    await client.storeCar(car);
+    const cid = root.toV0().toString();
+
+    res.json({ cid });
+  } catch (err) {
+    handle_error(res, req, `unexpected error calling /single endpoint: ${err}`);
+  }
+});
+
+app.post("/multiple", upload.array("assets", 2000), async function (req, res) {
+  try {
+    if (req.files == null || req.files.length == 0) {
+      return handle_error(res, req, "Invalid request: 'files' is missing or empty.", 400);
+    }
+    const cid = await client.storeDirectory(req.files.map((file) => new File([file.buffer], file.originalname)));
+
+    res.json({ cid: cid.toString() });
+  } catch (err) {
+    handle_error(res, req, `unexpected error calling /multiple endpoint: ${err}`);
+  }
+});
+
+app.listen(port);
